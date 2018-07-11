cec1b1b1412b394aa8a8e1d54f7213738bf99d44
Alan Tang <alantang888@users.noreply.github.com>
2018-07-11 18:15:56 +0800

    Print HTTP header instead of cookie

--- simple_http_request_display.go
+++ simple_http_request_display.go
@@ -10,9 +10,17 @@ import (
 func requestHandler (w http.ResponseWriter, r *http.Request){
 	buffer := bytes.Buffer{}
 	buffer.WriteString(fmt.Sprintf("Client from %v, request path: %v\n", r.RemoteAddr, r.URL.Path))
-	for _, cookie := range r.Cookies(){
-		buffer.WriteString(fmt.Sprintf("\tCOOKIE[\"%v\"]: %v\n", cookie.Name, cookie.Value))
+
+	//buffer.WriteString("Cookie:\n")
+	//for _, cookie := range r.Cookies(){
+	//	buffer.WriteString(fmt.Sprintf("\tCOOKIE[\"%v\"]: %v\n", cookie.Name, cookie.Value))
+	//}
+
+	buffer.WriteString("Header:\n")
+	for key, value := range r.Header{
+		buffer.WriteString(fmt.Sprintf("\tHeader[\"%v\"]: %v\n", key, value))
 	}
+
 	message := buffer.String()
 	log.Printf(message)
 	fmt.Fprintf(w, message)
