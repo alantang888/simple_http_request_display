4d1bb83bbb9d5b6f3bec718233eff9a620d90a2b
Alan Tang <alantang888@users.noreply.github.com>
2018-08-13 11:02:11 +0800

    Print both header and cookie on output
    Also add sample value for helm install.

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 [![Docker Build](https://img.shields.io/docker/build/alantang888/simple_http_request_display.svg)][docker-hub]
 [![Docker Pulls](https://img.shields.io/docker/pulls/alantang888/simple_http_request_display.svg?maxAge=604800)][docker-hub]
 
-This is a very simple http echo program to listen a TCP port 8080 and return requester IP:Port, request path and cookie. Also log those information in log.
+This is a very simple http echo program to listen a TCP port 8080 and return requester IP:Port, request path, headers and cookies. Also log those information in log.
 But not work behide proxy, it will return proxy address.
 
 There has a docker image [`alantang888/simple_http_request_display`][docker-hub]
+
+### Install by Helm
+You can install this by `helm install -f helm_chart/sample_vaule.yaml -n http-echo-test helm_chart/simple_http_request_display`.
+
+Please remember change the ingress hosts value.
 
 [docker-hub]: https://hub.docker.com/r/alantang888/simple_http_request_display/
--- simple_http_request_display.go
+++ simple_http_request_display.go
@@ -4,21 +4,31 @@ import (
 	"net/http"
 	"log"
 	"fmt"
-	"bytes"
+	"strings"
 )
 
 func requestHandler (w http.ResponseWriter, r *http.Request){
-	buffer := bytes.Buffer{}
+	//buffer := bytes.Buffer{}
+	buffer := strings.Builder{}
 	buffer.WriteString(fmt.Sprintf("Client from %v, request path: %v\n", r.RemoteAddr, r.URL.Path))
+	buffer.WriteString("==================================================================\n\n")
 
-	//buffer.WriteString("Cookie:\n")
-	//for _, cookie := range r.Cookies(){
-	//	buffer.WriteString(fmt.Sprintf("\tCOOKIE[\"%v\"]: %v\n", cookie.Name, cookie.Value))
-	//}
+	cookies := r.Cookies()
+	if len(cookies) > 0 {
+		buffer.WriteString("Cookie:\n")
+		for _, cookie := range cookies {
+			buffer.WriteString(fmt.Sprintf("\tCOOKIE[\"%v\"]: %v\n", cookie.Name, cookie.Value))
+		}
+		buffer.WriteString("==================================================================\n\n")
+	}
 
-	buffer.WriteString("Header:\n")
-	for key, value := range r.Header{
-		buffer.WriteString(fmt.Sprintf("\tHeader[\"%v\"]: %v\n", key, value))
+	headers := r.Header
+	if len(headers) > 0 {
+		buffer.WriteString("Header:\n")
+		for key, value := range headers {
+			buffer.WriteString(fmt.Sprintf("\tHeader[\"%v\"]: %v\n", key, value))
+		}
+		buffer.WriteString("==================================================================\n\n")
 	}
 
 	message := buffer.String()
@@ -27,6 +37,8 @@ func requestHandler (w http.ResponseWriter, r *http.Request){
 }
 
 func main() {
+	listenPort := 8080
+	fmt.Printf("Will try to listen on http://0.0.0.0:%[1]d. You may try on http://127.0.0.1:%[1]d", listenPort)
 	http.HandleFunc("/", requestHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", listenPort), nil))
 }
